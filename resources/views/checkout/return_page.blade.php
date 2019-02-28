41ec291c2bdabe1e683669484a65eeed693b707e
Chao-Yee Hsu <chaoyee22@gmail.com>
2019-03-01 02:33:30 +0800

    Minor fixes

--- resources/views/checkout/return_page.blade.php
+++ resources/views/checkout/return_page.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div>
   	<center>
-    <img src="storage/app/public/images/order_confirmation.gif" class="img-responsive" alt="" />
+    <img src="storage/app/public/images/order_confirmation.gif" alt="" />
       <h2><b>Thank You!</b> Your order has been received and confirmed.</h2>
       <p>Your order number is: xxxxxx.</p>
       <h3><a href="{{ route('index') }}">Please go back to the store! 
